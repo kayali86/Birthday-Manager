9b1eca8b85dbbc9778000926aa486d00a9b58e31
KAYALI <105053477+A-KAYALI@users.noreply.github.com>
2022-05-13 11:09:43 +0200

    Add firebase dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -48,6 +52,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  final _auth = FirebaseAuth.instance;
+
   int _counter = 0;
 
   void _incrementCounter() {
